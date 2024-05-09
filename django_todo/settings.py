73136afdcdb842a85ce76c5bb6d34de05bdfe0cf
amandakoka <amandakoka@icloud.com>
2024-05-09 09:39:56 +0100

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['git.heroku.com/ak-django-todo-app.git']
 
 
 # Application definition
